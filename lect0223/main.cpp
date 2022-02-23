54b8e02ce722c0bb9a317958627334e0d26bb300
kapsitis <kalvis.apsitis@gmail.com>
2022-02-23 15:56:19 +0200

    priority queue

--- lect0223/main.cpp
+++ lect0223/main.cpp
@@ -1,4 +1,5 @@
 //#include <vector>
+#include <list>
 #include <iostream>
 #include <string>
 
@@ -8,9 +9,10 @@ class Point {
 public:
     int x, y;
     string name;
+    list<int> myList;
     Point(int x1, int y1) { x = x1; y = y1; }
     void print() {
-        cout << "(" << x << ", " << y << ")" << endl;
+        cout << "(" << x << ", " << y << ", " << name << "," << myList.front() << ")" << endl;
     }
 };
 
@@ -34,6 +36,10 @@ public:
     {
         for (int i = 0; i < sz; ++i)
             elem[i] = 0;
+    }
+
+    void assign(int pos, double val) {
+        elem[pos] = val;
     }
 
     ~vector() { delete[] elem; }
@@ -69,14 +75,24 @@ int main()
 
     vector v2{1.0, 2.0, 3.0};
     v2.print();
+ 
+    vector v3; 		
+	v3 = v2; 
+    v3.assign(2, 113.0); // 3.0 -> 113
+    v3.print();
+
+    v2.print(); // did this change as well?
 
 
-    Point p1(3, 4);
-    p1.name = "Name1";
-    Point p2 = p1; 
-    p2.y = 17; // does this also change p1? 
-    p2.name[3] = '7';
 
-    p1.print(); // (3,4) or (3,17)? 
-    p2.print(); // (3, 17) - for sure
+    // Point p1(3, 4);
+    // p1.name = "Name1";
+    // p1.myList = list<int>{1,2};
+    // Point p2 = p1;  // p2 = (3,4)
+    // p2.y = 17; // does this also change p1 - NO! p1 still = (3,4)
+    // p2.name[3] = 'Z';
+    // p2.myList.front() = 117;  // it's deep copy. 
+
+    // p1.print(); // (3,4) 
+    // p2.print(); // (3, 17) - for sure
 }
